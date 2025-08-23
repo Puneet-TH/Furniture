d66d92f84eea60150d2678e3f543a6a1372c4680
thakurtushar09 <thakurtushar1309@gmail.com>
2025-08-23 23:33:58 +0530

    finishing backend

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from 'dotenv'
+import dbConnect from "./lib/dbConnect.js";
+import cookieParser from "cookie-parser";
+import userRoute from './Routes/user.routes.js';
+import enquiryRoute from './Routes/enquiry.routes.js';
+dotenv.config();
+dbConnect();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+app.get("/",(req,res)=>{
+    res.json({
+        message:true
+    })
+})
+
+
+
+app.use("/api/v1/user",userRoute);
+app.use("/api/v1/enquiry",enquiryRoute);
+
+
+
+
+app.listen(3000,()=>{
+    console.log("server running on 3000");
+})
