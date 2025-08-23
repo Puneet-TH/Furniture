b308eedf47c9477b387c467f18f335c1f52073c1
thakurtushar09 <thakurtushar1309@gmail.com>
2025-08-23 23:39:37 +0530

    allowing cors

--- server/index.js
+++ server/index.js
@@ -1,32 +1,34 @@
 import express from "express";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import dbConnect from "./lib/dbConnect.js";
 import cookieParser from "cookie-parser";
-import userRoute from './Routes/user.routes.js';
-import enquiryRoute from './Routes/enquiry.routes.js';
+import cors from "cors";   // ✅ import cors
+import userRoute from "./Routes/user.routes.js";
+import enquiryRoute from "./Routes/enquiry.routes.js";
+
 dotenv.config();
 dbConnect();
 const app = express();
+app.use(cors({
+  origin: "*",   
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.json({
+    message: true
+  });
+});
 
-app.get("/",(req,res)=>{
-    res.json({
-        message:true
-    })
-})
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/enquiry", enquiryRoute);
 
-
-
-app.use("/api/v1/user",userRoute);
-app.use("/api/v1/enquiry",enquiryRoute);
-
-
-
-
-app.listen(3000,()=>{
-    console.log("server running on 3000");
-})
+app.listen(3000, () => {
+  console.log("server running on 3000");
+});
